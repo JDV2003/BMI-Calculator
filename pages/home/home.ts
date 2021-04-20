62d8b3c73d7cd02ff13125068ebf8c82a260af62
JDV2003 <jaydee.villareal090013@my.lsgh.edu.ph>
2021-04-20 11:44:53 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -6,9 +6,29 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  height: number;
+  weight: number;
+  bmi: number;
+  message: string;
 
   constructor(public navCtrl: NavController) {
 
   }
+calculateBMI() {
+  this.bmi = this.weight / (this.height * this.height);
+  this.bmi = parseFloat(this.bmi.toFixed(2))
+
+  if (this.bmi < 18.5) {
+    this.message = "Underweight";
+  } else if (this.bmi >= 18.5 && this.bmi < 25){
+    this.message = "Normal";
+  } else if (this.bmi>=25 && this.bmi<30){
+    this.message = "Overweight";
+  } else {
+    this.message = "Obese";
+  }
+  
+}
+  
 
 }
